c89f9cc5a6fa0285a9727f31b22267aba7104eae
Juha <viderus@gmail.com>
2023-01-20 17:31:25 +0200

    Added browserRouter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-
 import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from 'react-router-dom'
+import Login from './views/Login';
+import Profile from './views/Profile';
+import Translate from './views/Translate';
 
 function App() {
+
   return (
-    <div className="App">
-      hello
-    </div>
-  );
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={ <Login/> } />
+          <Route path="/profile" element={ <Profile/>} />
+          <Route path="/translate" element={ <Translate/> } />
+        </Routes>
+      </div>
+    </BrowserRouter>
+
+  )
 }
 
-export default App;
+export default App
