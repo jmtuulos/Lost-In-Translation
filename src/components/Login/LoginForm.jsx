dc7e548c6707e8ebc3fb79be8d628479b05a1db3
Juha <viderus@gmail.com>
2023-01-24 16:44:31 +0200

    Added loginForm to App

--- src/components/Login/LoginForm.jsx
+++ src/components/Login/LoginForm.jsx
@@ -1,19 +1,46 @@
+import { useEffect, useState } from 'react'
 import { useForm } from 'react-hook-form'
-import { useState } from 'react'
-import checkForUser from '../../api/user.js'
-
+import { loginUser } from '../../api/user.js'
+import { storageSave } from '../../utils/storage.js'
+import { useNavigate } from 'react-router-dom'
+import { useUser } from '../../context/UserContext.jsx'
+import { STORAGE_KEY_USER } from '../../const/storageKeys.js'
 
 const LoginForm = () => {
+
 	const { register, handleSubmit } = useForm()
-	// const [data1, setData] = useState("")
+	const { user, setUser } = useUser()
+	const navigate = useNavigate()
+
+	const onSubmit = async (data) => {
+		setLoading(true)
+		const [error, userResponse] = await loginUser(data.name)
+		if (error !== null)
+			setApiError(error)
+		if (userResponse !== null){
+			storageSave(STORAGE_KEY_USER, userResponse)
+			setUser(userResponse)
+		}
+		setLoading(false)
+	}
+
+	const [ loading, setLoading ] = useState(false)
+	const [ apiError, setApiError ] = useState(null)
+
+	useEffect(() => {
+		if (user !== null)
+			navigate('/translate')
+	}, [ user, navigate ])
 
 	return (
-		<form onSubmit={ handleSubmit((data) => checkForUser(data))}>
+		<form onSubmit={ handleSubmit(onSubmit) }>
 			<input {...register("name")} type="text"
 			minLength="3"
 			placeholder="Enter username"
 			required/>
-			<input type="submit"/>
+			<input disabled={ loading } type="submit"/>
+			{loading && <p>Logging in</p>}
+			{ apiError && <p>{ apiError }</p>}
 		</form>
 	)
 }
